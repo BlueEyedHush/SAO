766fb2d91c2c1fe03e513b9ecc9eb57bb82c3477
piotroramus <piotroramus@mail.com>
2017-03-28 19:31:30 +0200

    Refactored simulation visualization, added labeling

--- visualize.py
+++ visualize.py
@@ -15,6 +15,34 @@ def on_key(event, args):
         exit(0)
 
 
+def draw_graph(graph, untouched_nodes, burning_nodes, defended_nodes, edges, positions, labels, solution):
+    node_size = 300
+
+    plt.clf()
+    plt.axis('off')
+    plt.title('Solution: ' + str(solution))
+    nx.draw_networkx_nodes(graph, pos=positions,
+                           nodelist=burning_nodes,
+                           node_size=node_size,
+                           node_color='y')
+
+    nx.draw_networkx_nodes(graph,
+                           pos=positions,
+                           nodelist=defended_nodes,
+                           node_size=node_size,
+                           node_color='b')
+
+    nx.draw_networkx_nodes(graph,
+                           pos=positions,
+                           nodelist=untouched_nodes,
+                           node_size=node_size,
+                           node_color='r')
+
+    nx.draw_networkx_edges(graph, pos=positions, edgelist=edges)
+    nx.draw_networkx_labels(graph, pos=positions, labels=labels, font_size=12)
+    plt.show()
+
+
 def draw_next_step(args):
     print "Drawing next step..."
 
@@ -35,44 +63,24 @@ def draw_next_step(args):
                 args['defended_nodes'].append(node_id)
                 args['untouched_nodes'].remove(node_id)
 
-        plt.clf()
-        plt.axis('off')
-
-        nx.draw_networkx_nodes(args['graph'],
-                               pos=args['positions'],
-                               nodelist=args['burning_nodes'],
-                               node_size=args['node_size'],
-                               node_color='y')
-
-        nx.draw_networkx_nodes(args['graph'],
-                               pos=args['positions'],
-                               nodelist=args['defended_nodes'],
-                               node_size=args['node_size'],
-                               node_color='b')
-
-        nx.draw_networkx_nodes(args['graph'],
-                               pos=args['positions'],
-                               nodelist=args['untouched_nodes'],
-                               node_size=args['node_size'],
-                               node_color='r')
-
-        nx.draw_networkx_edges(args['graph'], pos=args['positions'], edgelist=args['edges'], node_color='r')
-
-        plt.show()
+        draw_graph(args['graph'], args['untouched_nodes'], args['burning_nodes'], args['defended_nodes'], args['edges'],
+                   args['positions'], args['labels'], args['solution'])
 
 
 def draw_previous_step(args):
     print "Not implemented yet :("
 
 
-def visualize_simulation(graph, transitions):
-    NODE_SIZE = 300
+def visualize_simulation(graph, transitions, solution):
+    nx_graph = nx.Graph()
 
-    G = nx.Graph()
     edges = graph.get_edges()
-    G.add_edges_from(edges)
-    positions = nx.spring_layout(G)
-    nx.draw(G, pos=positions)
+    nx_graph.add_edges_from(edges)
+    positions = nx.spring_layout(nx_graph)
+
+    labels = dict()
+    for node_id in graph.nodes:
+        labels[node_id] = node_id
 
     shown_transitions = list()
 
@@ -81,7 +89,7 @@ def visualize_simulation(graph, transitions):
     defended_nodes = list()
 
     args = {
-        'graph': G,
+        'graph': nx_graph,
         'edges': edges,
         'positions': positions,
         'future_transitions': transitions,
@@ -89,9 +97,9 @@ def visualize_simulation(graph, transitions):
         'untouched_nodes': untouched_nodes,
         'burning_nodes': burning_nodes,
         'defended_nodes': defended_nodes,
-        'node_size': NODE_SIZE,
+        'labels': labels,
+        'solution': solution,
     }
 
     plt.gcf().canvas.mpl_connect('key_press_event', lambda event: on_key(event, args))
-    plt.axis('off')
-    plt.show()
+    draw_graph(nx_graph, graph.nodes, None, None, edges, positions, labels, solution)
