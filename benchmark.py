996de42ecd06022b566a5417ebc648abd47eca64
knawara <blueeyedhush@gmail.com>
2017-04-05 14:33:44 +0200

    renaming graph_per_iterm, making it non-per-test-config variable

--- benchmark.py
+++ benchmark.py
@@ -2,26 +2,27 @@
 
 import itertools
 import logging
-
 from generate import load_graph
 from solvers import simple_genetic_crossover
 
 
-def benchmark(algorithm, test_configurations):
+def benchmark(algorithm, test_configurations, iterations):
     """
 
     :param algorithm: function taking 2 arguments: 1. graph 2. nodes where fire starts and returning best solution
-    :param test_configuration: list of tuples
+    :param test_configuration: list of tuples (vertex_no, graph_density, ffs_per_step, algo_iters)
+    :param iterations: how many times whole algorithm will be repeated with identical parameters
+        average value & relative stddev value'll be reported
     :return:
     """
 
-    for vertex_no, density, iters_per_graph, ffs_per_step, algo_iters in test_configurations:
+    for vertex_no, density, ffs_per_step, algo_iters in test_configurations:
         g = load_graph(vertex_no, density)
         sn = map(lambda v: v.id, g.get_starting_nodes())
 
-        print u"VERTEX_NO = {}, DENSITY = {}, STARTING_NODES = {}, ITERS_PER_GRAPH = {}, FFS_PER_STEP = {}, ALGO_ITERS = {}" \
-            .format(vertex_no, density, sn, iters_per_graph, ffs_per_step, algo_iters)
-        for i in xrange(iters_per_graph):
+        print u"VERTEX_NO = {}, DENSITY = {}, STARTING_NODES = {}, FFS_PER_STEP = {}, ALGO_ITERS = {}" \
+            .format(vertex_no, density, sn, ffs_per_step, algo_iters)
+        for i in xrange(iterations):
             solution, score = algorithm(g, sn, iter_no=algo_iters, ffs_per_step=ffs_per_step)
             print score
 
@@ -33,8 +34,7 @@ if __name__ == "__main__":
     DENSITIES = [0.1, 0.5, 0.9]
     ALGO_ITERS = [5, 50, 100]
     FFS_PER_STEP = [1, 3, 5]
-    ITERS_PER_GRAPH = [5]
 
-    confs = itertools.product(V_NO, DENSITIES, ITERS_PER_GRAPH, FFS_PER_STEP, ALGO_ITERS)
+    confs = itertools.product(V_NO, DENSITIES, FFS_PER_STEP, ALGO_ITERS)
 
-    benchmark(simple_genetic_crossover, confs)
+    benchmark(simple_genetic_crossover, confs, 5)
