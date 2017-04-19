c193dd1f8363ff75bf609cf1892aed2e49f4079e
piotroramus <piotroramus@mail.com>
2017-04-19 16:18:13 +0200

    Implemented rank selection

--- selection.py
+++ selection.py
@@ -10,6 +10,24 @@ def evaluate(chromosome, environment):
     return simulation(graph, chromosome, init_nodes, ff_per_step)
 
 
+def _roulette_select(weighted_population, k):
+    """ Returns k individual with probability proportional to assigned weights """
+
+    weights_sum = sum(fitness for _, fitness in weighted_population)
+
+    result = list()
+    for _ in xrange(k):
+        pick = random.uniform(0, weights_sum)
+        current = 0
+        for p, fitness in weighted_population:
+            current += fitness
+            if current > pick:
+                result.append(p)
+                break
+
+    return result
+
+
 def tournament_selection(population, k, environment):
     """
 
@@ -42,19 +60,7 @@ def roulette_wheel_selection(population, k, environment):
     """
 
     weights = [(p, evaluate(p, environment)[2]) for p in population]
-    weights_sum = sum(fitness for _, fitness in weights)
-
-    result = list()
-    for _ in xrange(k):
-        pick = random.uniform(0, weights_sum)
-        current = 0
-        for p, fitness in weights:
-            current += fitness
-            if current > pick:
-                result.append(p)
-                break
-
-    return result
+    return _roulette_select(weights, k)
 
 
 def rank_selection(population, k, environment):
@@ -65,4 +71,10 @@ def rank_selection(population, k, environment):
     :param environment: a tuple (graph, init_nodes, ff_per_step) describing environment in which the solution is to be evaluated
     :return:
     """
-    raise NotImplementedError()
+
+    sorted_by_fitness = sorted(population, key=lambda p: evaluate(p, environment)[2])
+    ranks = xrange(1, len(population) + 1)
+
+    population_with_rank = zip(sorted_by_fitness, ranks)
+
+    return _roulette_select(population_with_rank, k)
