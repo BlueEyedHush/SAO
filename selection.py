793be9f984ee9473ac19febeee799bda67e62dc4
piotroramus <piotroramus@mail.com>
2017-04-19 14:58:18 +0200

    Implemented tournament selection

--- selection.py
+++ selection.py
@@ -1,5 +1,7 @@
 import random
 
+from heapq import nlargest
+
 from simulation import simulation
 
 
@@ -16,9 +18,18 @@ def tournament_selection(population, k, environment):
     :param environment: a tuple (graph, init_nodes, ff_per_step) describing environment in which the solution is to be evaluated
     :return:
     """
-    random_k_samples = sorted(random.sample(population, k), key=lambda p: evaluate(p, environment))
 
-    print random_k_samples
+    result = list()
+    for _ in xrange(k):
+        # select some random subpopulation
+        subpopulation_size = random.randint(1, len(population) - 1)
+        random_samples = sorted(random.sample(population, subpopulation_size))
+
+        # get the best individual from the subpopulation
+        winner = nlargest(1, random_samples, key=lambda p: evaluate(p, environment))
+        result.append(winner[0])
+
+    return result
 
 
 def roulette_wheel_selection(population, k, environment):
