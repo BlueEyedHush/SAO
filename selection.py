9aa01c795839c7799f47091a30f198eff7ca43de
piotroramus <piotroramus@mail.com>
2017-04-19 15:50:44 +0200

    Roulette wheel selection implemented

--- selection.py
+++ selection.py
@@ -26,7 +26,7 @@ def tournament_selection(population, k, environment):
         random_samples = sorted(random.sample(population, subpopulation_size))
 
         # get the best individual from the subpopulation
-        winner = nlargest(1, random_samples, key=lambda p: evaluate(p, environment))
+        winner = nlargest(1, random_samples, key=lambda p: evaluate(p, environment)[2])
         result.append(winner[0])
 
     return result
@@ -40,7 +40,21 @@ def roulette_wheel_selection(population, k, environment):
     :param environment: a tuple (graph, init_nodes, ff_per_step) describing environment in which the solution is to be evaluated
     :return:
     """
-    raise NotImplementedError()
+
+    weights = [(p, evaluate(p, environment)[2]) for p in population]
+    weights_sum = sum(fitness for _, fitness in weights)
+
+    result = list()
+    for _ in xrange(k):
+        pick = random.uniform(0, weights_sum)
+        current = 0
+        for p, fitness in weights:
+            current += fitness
+            if current > pick:
+                result.append(p)
+                break
+
+    return result
 
 
 def rank_selection(population, k, environment):
