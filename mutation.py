a959f48f8fdc23ac9b1188df88d61699108d194f
piotroramus <piotroramus@mail.com>
2017-04-20 10:31:04 +0200

    Rendom slide mutation implemented

--- mutation.py
+++ mutation.py
@@ -25,7 +25,39 @@ def inversion_mutation(chromosome):
 
 
 def random_slide_mutation(chromosome):
-    raise NotImplementedError()
+    """ Draw a random swath (range) and 'slide' it left or right """
+
+    size = len(chromosome)
+    max_swath_factor = 0.6
+    swath_size = random.randint(1, int(size * max_swath_factor))
+
+    left = 0
+    right = 1
+    directions = (left, right)
+    slide_direction = random.choice(directions)
+
+    if slide_direction == left:
+        # leave at least one position to left-slide
+        a = random.randint(1, size - swath_size)
+        b = a + swath_size - 1
+        slide_length = - random.randint(1, a)
+    else:
+        # leave at least 1 position to right-slide
+        b = random.randint(swath_size - 1, size - 2)
+        a = b - swath_size + 1
+        slide_length = random.randint(1, size - b - 1)
+
+    # x, y correspond to a, b after slide
+    x = a + slide_length
+    y = b + slide_length
+
+    # the slide of (a,b) includes a and b
+    if slide_direction == left:
+        mutated = chromosome[:x] + chromosome[a:b + 1] + chromosome[x:a] + chromosome[b + 1:]
+    else:
+        mutated = chromosome[:a] + chromosome[b + 1:y + 1] + chromosome[a:b + 1] + chromosome[y + 1:]
+
+    return mutated
 
 
 def random_swap_mutation(chromosome):
@@ -40,3 +72,5 @@ def single_swap_mutation(chromosome):
     i, j = random.sample(range(len(chromosome)), 2)
     chromosome[i], chromosome[j] = chromosome[j], chromosome[i]
     return chromosome
+
+# TODO: is there an adjacent swap mutation???
