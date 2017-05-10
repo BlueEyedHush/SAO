d692ddc6a8813676f32411a98da46e3343457cef
piotroramus <piotroramus@mail.com>
2017-05-10 08:39:55 +0200

    Generic plot drawing

--- evaluate.py
+++ evaluate.py
@@ -1,15 +1,51 @@
 import csv
+import os
 
 import matplotlib.pyplot as plt
 
 from solvers import run_framework
 
 
-def prepare_configs(iters, ffs, graph_desc, input_file):
+def _build_cfg_filepath(config, type='csv', plot_category=None):
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    base_dir = os.path.join(script_dir, 'results', config['graph_desc'])
+
+    filebase = "{}_{}_{}_{}_{}".format(
+        config['population_size'],
+        config['selection'],
+        config['crossover'],
+        config['mutation'],
+        config['graph_desc']
+    )
+
+    if type == 'csv':
+        filename = "{}.csv".format(filebase)
+        dir_path = os.path.join(base_dir, 'csv')
+    elif type == 'plot':
+        filename = "{}.png".format(filebase)
+        dir_path = os.path.join(base_dir, 'plots', plot_category)
+    else:
+        raise ValueError('Invalid filepath type')
+
+    if not os.path.exists(dir_path):
+        print "{} does not exists. Creating one...".format(dir_path)
+        os.makedirs(dir_path)
+
+    full_path = os.path.join(dir_path, filename)
+    return full_path
+
+
+def get_possible_factors():
     population_sizes = [10, 20, 50]
     selection_ops = ['tournament', 'roulette', 'rank']
     crossover_ops = ['cycle', 'injection', 'multi_injection', 'pmx', 'single_pmx']
     mutation_ops = ['adjacent_swap', 'insertion', 'inversion', 'slide', 'random_swap', 'scramble', 'single_swap']
+
+    return population_sizes, selection_ops, crossover_ops, mutation_ops
+
+
+def generate_configs(iters, ffs, graph_desc, input_file):
+    population_sizes, selection_ops, crossover_ops, mutation_ops = get_possible_factors()
 
     # generate all possible combinations of defined operators and population size
     configs = list()
@@ -36,8 +72,8 @@ def calculate_results(configs):
     configs_num = len(configs)
     processed_config_num = 1
     for cfg in configs:
-        csv_file = "results/{}_{}_{}_{}_{}.csv".format(cfg['population_size'], cfg['selection'], cfg['crossover'],
-                                                       cfg['mutation'], cfg['graph_desc'])
+        csv_file = _build_cfg_filepath(cfg, type='csv')
+
         print "[{}/{}] Calculating {}...".format(processed_config_num, configs_num, csv_file)
         run_framework(
             loggers='',
@@ -53,28 +89,125 @@ def calculate_results(configs):
         processed_config_num += 1
 
 
-def draw_plots(configs):
-    # x = []
-    # y = []
-    #
-    #     with open(csv_file, 'rb') as csvfile:
-    #         plots = csv.DictReader(csvfile, delimiter=',', )
-    #         for row in plots:
-    #             x.append(int(row['iter_no']))
-    #             y.append(float(row['max_saved']))
-    #
-    #     keys[size] = x
-    #     values[size] = y
-    #
-    # for size in population_sizes:
-    #     plt.plot(keys[size], values[size], label=size)
-    #
-    # plt.xlabel('Iteration')
-    # plt.ylabel('Saved %')
-    # plt.legend()
-    # plt.show()
+def generate_plot_profiles():
+    population_sizes, selection_ops, crossover_ops, mutation_ops = get_possible_factors()
 
-    pass
+    profiles = list()
+    # profiles for population_size
+    for so in selection_ops:
+        for co in crossover_ops:
+            for mo in mutation_ops:
+                profiles.append({
+                    'population_size': None,
+                    'selection': so,
+                    'crossover': co,
+                    'mutation': mo,
+                })
+
+    # profiles for selection
+    for ps in population_sizes:
+        for co in crossover_ops:
+            for mo in mutation_ops:
+                profiles.append({
+                    'population_size': ps,
+                    'selection': None,
+                    'crossover': co,
+                    'mutation': mo,
+                })
+
+    # profiles for crossover
+    for ps in population_sizes:
+        for so in selection_ops:
+            for mo in mutation_ops:
+                profiles.append({
+                    'population_size': ps,
+                    'selection': so,
+                    'crossover': None,
+                    'mutation': mo,
+                })
+
+    # profiles for mutation
+    for ps in population_sizes:
+        for so in selection_ops:
+            for co in crossover_ops:
+                profiles.append({
+                    'population_size': ps,
+                    'selection': so,
+                    'crossover': co,
+                    'mutation': None,
+                })
+
+    return profiles
+
+
+def draw_plots(plot_profile, configs):
+    categories = list()
+    for plot_label in plot_profile:
+        if plot_profile[plot_label]:
+            categories.append(plot_label)
+    if len(categories) != 3:
+        raise ValueError('Invalid plot profile - exactly one category should be left empty')
+
+    profiled_category = [cat for cat in plot_profile.keys() if cat not in categories][0]
+
+    # find only results matching criteria in plot_profile
+    matched_results = list()
+    for cfg in configs:
+        matching = True
+        for plot_label in categories:
+            if cfg[plot_label] != plot_profile[plot_label]:
+                matching = False
+                break
+        if matching:
+            matched_results.append(cfg)
+
+    xdata = dict()
+    ydata = dict()
+    plt.clf()
+    for cfg in matched_results:
+
+        plot_label = cfg[profiled_category]
+        x = []
+        y = []
+
+        csv_file = _build_cfg_filepath(cfg, type='csv')
+
+        with open(csv_file, 'rb') as csvfile:
+            plots = csv.DictReader(csvfile, delimiter=',')
+            for row in plots:
+                x.append(int(row['iter_no']))
+                y.append(float(row['max_saved']))
+
+        xdata[plot_label] = x
+        ydata[plot_label] = y
+
+    if matched_results:
+        for plot_label in xdata:
+            plt.plot(xdata[plot_label], ydata[plot_label], label=plot_label)
+
+        title = "{};{};{};{};{}".format(
+            plot_profile['population_size'],
+            plot_profile['selection'],
+            plot_profile['crossover'],
+            plot_profile['mutation'],
+            matched_results[0]['graph_desc'],
+        )
+        plt.xlabel('Iteration')
+        plt.ylabel('Saved %')
+        plt.title(title)
+        plt.legend()
+
+        plot_path = _build_cfg_filepath(
+            {
+                'population_size': plot_profile['population_size'],
+                'selection': plot_profile['selection'],
+                'crossover': plot_profile['crossover'],
+                'mutation': plot_profile['mutation'],
+                'graph_desc': matched_results[0]['graph_desc']
+            },
+            type='plot',
+            plot_category=profiled_category)
+        plt.savefig(plot_path)
 
 
 if __name__ == '__main__':
@@ -82,5 +215,12 @@ if __name__ == '__main__':
     graph_desc = '80_035_2'
     input_file = 'graphs/80_035_2.rgraph'
     ffs = 3
-    configs = prepare_configs(iters, ffs, graph_desc, input_file)
+    configs = generate_configs(iters, ffs, graph_desc, input_file)
     calculate_results(configs)
+
+    plot_profiles = generate_plot_profiles()
+    for i, plot_profile in enumerate(plot_profiles):
+        print "Drawing plot {} out of {}".format(i, len(plot_profiles))
+        draw_plots(plot_profile, configs)
+
+    # TODO: why do I have 318 profiles and 315 configs?
